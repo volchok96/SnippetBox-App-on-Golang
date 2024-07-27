1937ea75306e676cfa0dd58813cbb35769077d75
Katerina Zakharova <kzakharova96@yandex.com>
2024-07-27 23:02:23 +0300

    added neutered filesystem

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "log"
+    "net/http"
+    "path/filepath"
 )
 
 func main() {
-	// Register two new handlers and their corresponding URL patterns in
-	// the servemux router
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+    mux := http.NewServeMux()
 
-	log.Println("Running the web server on http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+    fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
+    mux.Handle("/static", http.NotFoundHandler())
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+    err := http.ListenAndServe(":4000", mux)
+    log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+    fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+    f, err := nfs.fs.Open(path)
+    if err != nil {
+        return nil, err
+    }
+
+    s, err := f.Stat()
+    if s.IsDir() {
+        index := filepath.Join(path, "index.html")
+        if _, err := nfs.fs.Open(index); err != nil {
+            closeErr := f.Close()
+            if closeErr != nil {
+                return nil, closeErr
+            }
+
+            return nil, err
+        }
+    }
+
+    return f, nil
 }
