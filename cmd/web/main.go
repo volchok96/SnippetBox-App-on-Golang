df3bb747ecba2486dcac028633fa1142fe8db3b2
Katerina Zakharova <kzakharova96@yandex.com>
2024-07-28 14:37:57 +0300

    configured app from the command line

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,44 +1,52 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "path/filepath"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-    mux := http.NewServeMux()
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
-    fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
-    mux.Handle("/static", http.NotFoundHandler())
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
 }
 
 type neuteredFileSystem struct {
-    fs http.FileSystem
+	fs http.FileSystem
 }
 
 func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-    f, err := nfs.fs.Open(path)
-    if err != nil {
-        return nil, err
-    }
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
 
-    s, err := f.Stat()
-    if s.IsDir() {
-        index := filepath.Join(path, "index.html")
-        if _, err := nfs.fs.Open(index); err != nil {
-            closeErr := f.Close()
-            if closeErr != nil {
-                return nil, closeErr
-            }
-
-            return nil, err
-        }
-    }
-
-    return f, nil
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+			return nil, &os.PathError{Op: "open", Path: path, Err: os.ErrPermission}
+		}
+		return nil, err
+	}
+	return f, nil
 }
