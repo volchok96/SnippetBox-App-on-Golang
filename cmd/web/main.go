4cc057e78229b201157252077c23a544f7d3975d
Katerina Zakharova <kzakharova96@yandex.com>
2024-07-29 19:18:29 +0300

    created a pool of database connections + added extra security check

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/term"
 )
 
 type application struct {
@@ -13,13 +19,31 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
+	addr := flag.String("addr", ":4000", "Network address of the web server")
+	dsn := flag.String("dsn", "/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	// use flag "Lshortfile" to include the file name and line number
-	// where the error was detected in the log
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Prompt the user for the database password
+	fmt.Print("Enter database password: \n")
+	bytePassword, err := term.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	password := string(bytePassword)
+	password = strings.ReplaceAll(password, "\n", "")
+
+	// Construct the full DSN with the entered password
+	fullDSN := fmt.Sprintf("web:%s@%s", password, *dsn)
+
+	// Create a connection pool to the database
+	db, err := openDB(fullDSN)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -33,6 +57,19 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool
+// for the given data source name (DSN).
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module volchok96/SnippetBox-App-on-Golang.git
 go 1.22.2
 
 require (
+	github.com/go-sql-driver/mysql v1.8.1
+	golang.org/x/term v0.22.0
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	golang.org/x/sys v0.22.0 // indirect
 )
