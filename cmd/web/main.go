840a3845b7bd733b8fc363c10dfff3c609d0dc65
Katerina Zakharova <kzakharova96@yandex.com>
2024-08-14 15:32:19 +0300

    preparing app structure for Docker test

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/go-redis/redis/v8"
 	_ "github.com/go-sql-driver/mysql"
 	"volchok96.com/snippetbox/pkg/models/mysql"
-	"github.com/go-redis/redis/v8"
 )
 
 type application struct {
@@ -18,7 +20,7 @@ type application struct {
 	infoLog     *log.Logger
 	snippets    *mysql.SnippetModel
 	redisClient *redis.Client
-	tmplCache map[string]*template.Template
+	tmplCache   map[string]*template.Template
 }
 
 func main() {
@@ -30,16 +32,13 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// Prompt for the database password
-	password, err := promptForPassword()
-	if err != nil {
-		errorLog.Fatal(err)
-	}
+	// Set the database password here
+	const dbPassword = "my_password"
 
-	// Create the full DSN with the entered password
-	fullDSN := constructDSN(password, *dsn)
+	// Create the full DSN with the provided password
+	fullDSN := constructDSN(dbPassword, *dsn)
 
-	// Create a connection pool to the database
+	// Create a connection pool for the database
 	db, err := openDB(fullDSN)
 	if err != nil {
 		errorLog.Fatal(err)
@@ -53,30 +52,49 @@ func main() {
 	}
 	defer rdb.Close()
 
-
-	// Initialize new template cache
+	// Initialize a new template cache
 	tmplCache, err := newTemplateCache("./ui/html/")
 	if err != nil {
 		errorLog.Fatal(err)
 	}
 
-	// Adding app dependencies
+	// Add application dependencies
 	app := &application{
 		errorLog:    errorLog,
 		infoLog:     infoLog,
 		snippets:    &mysql.SnippetModel{DB: db},
-		redisClient: rdb, // Storing the Redis client in the application structure
-		tmplCache: tmplCache,
+		redisClient: rdb, // Storing the Redis client in the application struct
+		tmplCache:   tmplCache,
 	}
 
 	srv := &http.Server{
-		Addr:     *addr,
-		ErrorLog: errorLog,
-		Handler:  app.routes(),
+		Addr:              *addr,
+		ErrorLog:          errorLog,
+		Handler:           app.routes(),
 		ReadHeaderTimeout: 5 * time.Second,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
 	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+// constructDSN constructs the data source name (DSN) string with the entered password.
+func constructDSN(password, dsn string) string {
+	return fmt.Sprintf("my_user:%s@%s", password, dsn)
+}
+
+// openDB wraps sql.Open() and returns a sql.DB connection pool
+// for the specified data source name (DSN).
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.22.2
 require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/go-sql-driver/mysql v1.8.1
-	golang.org/x/term v0.22.0
 )
 
 require (
